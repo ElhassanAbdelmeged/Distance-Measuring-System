a54cf6fbb1a03627536040db4449c77514414734
El_Hassan_Abdel_Meged <hassanabdelmaged123@gmail.com>
2023-09-26 22:05:01 +0300

    1st version of Distance-Measuring-System  Project

---
+++ Mini_Project_4_WS/Mini_Project4/Debug/Mini_Project4.d
@@ -0,0 +1,14 @@
+Mini_Project4.o Mini_Project4.o: ../Mini_Project4.c ../icu.h \
+  ../std_types.h ../ultrasonic.h ../gpio.h ../lcd.h ../common_macros.h
+
+../icu.h:
+
+../std_types.h:
+
+../ultrasonic.h:
+
+../gpio.h:
+
+../lcd.h:
+
+../common_macros.h:
